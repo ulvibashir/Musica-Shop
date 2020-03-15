ee95497c345254ba43144bad8d5ec1197e6e761f
Ulvi Bashir <ubashirov@outlook.com>
2020-03-15 16:51:52 +0400

    Updates

---
+++ precache-manifest.ab46fed805f9aa04bcc0dcafff0e3bc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78757d1d166caf8b96348e39df90860f",
+    "url": "/Musica-Shop/index.html"
+  },
+  {
+    "revision": "cff7cd7b3364c4f10925",
+    "url": "/Musica-Shop/static/css/main.b36ad158.chunk.css"
+  },
+  {
+    "revision": "603226add7325da1ef09",
+    "url": "/Musica-Shop/static/js/2.b43275e2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Musica-Shop/static/js/2.b43275e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff7cd7b3364c4f10925",
+    "url": "/Musica-Shop/static/js/main.a35a7248.chunk.js"
+  },
+  {
+    "revision": "6f4aaae8597375e36363",
+    "url": "/Musica-Shop/static/js/runtime-main.2f9bf049.js"
+  }
+]);
